3f45d26664ccb270e4872a2944a4a7e316d8ae9d
miloosterman <miloosterman@protonmail.com>
2022-03-30 12:22:01 -0700

    Made computerPlay more compact

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function computerPlay(){
-    let gameSeed = Math.floor(Math.random * 3) + 1;
-    let gameResult;
+const choices = ["Rock", "Paper", "Scissors"];
 
-    switch (gameSeed) {
-        case 1:
-            return gameResult = "Rock";
-        case 2:
-            return gameResult = "Paper";
-        case 3:
-            return gameResult = "Scissors";
-        default:
-            throw new Exception(`Case ${gameSeed} not found.`);
-    }
+let computerSelection = computerPlay();
+let playerSelection = "rock";
+console.log(computerSelection);
+// playRound(playerSelection, computerSelection);
+
+function computerPlay(){
+    return choices[Math.floor(Math.random() * choices.length)];
+
+
 }
+
+// function playRound(playerSelection, computerSelection){
+    
+// }
