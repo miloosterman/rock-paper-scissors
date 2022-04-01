e1c77304fdba030c311433296ed3c1d07b378768
miloosterman <miloosterman@protonmail.com>
2022-04-01 09:39:13 -0700

    Added HTML doctype declaration, 4-2 space indents

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 const choices = {
-    Rock: {weakTo: "Paper", strongTo: "Scissors"},
-    Paper: {weakTo: "Scissors", strongTo: "Rock"},
-    Scissors: {weakTo: "Rock", strongTo: "Paper"}
+	Rock: {weakTo: "Paper", strongTo: "Scissors"},
+	Paper: {weakTo: "Scissors", strongTo: "Rock"},
+	Scissors: {weakTo: "Rock", strongTo: "Paper"}
 };
+
 const weapons = Object.keys(choices);
 
 game();
 
 function computerPlay(){
-    return weapons[Math.floor(Math.random() * weapons.length)];
+	return weapons[Math.floor(Math.random() * weapons.length)];
 }
 
 function getPlayerSelection(){
-    let playerSelection = prompt("Please enter your choice of rock/paper/scissors.");
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    while(!weapons.includes(playerSelection)){
-        playerSelection = prompt("That was not a valid selection, please choose from rock/paper/scissors");
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    }
-    
-    return playerSelection;
+	let playerSelection = prompt("Please enter your choice of rock/paper/scissors.");
+	playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+	while(!weapons.includes(playerSelection)){
+		playerSelection = prompt("That was not a valid selection, please choose from rock/paper/scissors");
+		playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+	}
+	
+	return playerSelection;
 }
 
 function playRound(playerSelection, computerSelection){
-    console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}.`)
-    if (choices[playerSelection].strongTo === computerSelection){
-        return `Winner! ${playerSelection} beats ${computerSelection}!`
-    }
+	console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}.`)
+	if (choices[playerSelection].strongTo === computerSelection){
+		return `Winner! ${playerSelection} beats ${computerSelection}!`
+	}
 
-    if (choices[playerSelection].weakTo === computerSelection){
-        return `You lose... ${computerSelection} beats ${playerSelection}`
-    }
+	if (choices[playerSelection].weakTo === computerSelection){
+		return `You lose... ${computerSelection} beats ${playerSelection}`
+	}
 
-    return `Tie game... No winner between ${playerSelection} and ${computerSelection}. How boring.`
+	return `Tie game... No winner between ${playerSelection} and ${computerSelection}. How boring.`
 }
 
 function game(){
-    for (let i = 0; i < 5; i++){
-        let computerSelection = computerPlay();
-        let playerSelection = getPlayerSelection();
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
-    }
+	for (let i = 0; i < 5; i++){
+		let computerSelection = computerPlay();
+		let playerSelection = getPlayerSelection();
+		let result = playRound(playerSelection, computerSelection);
+		console.log(result);
+	}
 }
